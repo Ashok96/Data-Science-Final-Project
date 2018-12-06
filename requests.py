55a3b38bced3bc4bb4cde04dbba55cf36a12cc78
Kymberlee Hill <kymberleehill@KymberleesMBP2.fios-router.home>
2018-12-05 19:56:40 -0500

    fixed request script to templateparameters for api request and added data file obtained from the API

--- requests.py
+++ requests.py
@@ -1,18 +1,23 @@
 """
-Filename: extract.py
+Filename: requests.py
+Purpose: To obtain data from USDA api endpoint and put it into a file
 Authors: Kymberlee Hill , Swarnim Bhandari
-Purpose: to obtain data from USDA api endpoint and put it into a file
 """
-import requests
-import json
+from urllib.request import urlopen
+from urllib.parse import urlencode
 
-api_key = "0D98036D-C6D4-390F-A3E4-C7169A050F15"
-response = requests.get("http://quickstats.nass.usda.gov/api/api_GET/?key="+ api_key +"&year__GE=1989&year__LE=2018&program=SURVEY&sector=\'ANIMALS & PRODUCTS\'&group=POULTRY&commodity_desc=TURKEYS&category=SLAUGHTERED&data_item__LIKE='F1 - SLAUGHTERED, MEASURED IN HEAD'&format=csv&state_alpha=VA")
+params = {
+    'api_key': '0D98036D-C6D4-390F-A3E4-C7169A050F15',
+    'short_description': "TURKEYS, YOUNG, SLAUGHTER, FI - SLAUGHTERED, MEASURED IN HEAD",
+    'year__GE' : '1989',
+    'year__LE' : '2018',
+    'state_alpha' : 'VA',
+    'frequency_description' : 'MONTHLY',
+    }
 
-bad_error = response.raise_for_status()
-status = response.status_code
-print(bad_error)
-print(status)
+url = "http://quickstats.nass.usda.gov/api/api_GET/?" + urlencode(params)
 
-with open('outputfile.json', 'w') as outf:
-  outf.write(response.text)
+with urlopen(url) as x:
+        data = x.read()
+        with open("outputdata.json", 'wb') as outf:
+            outf.write(data)
